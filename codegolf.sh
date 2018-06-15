1fcd4d5b3b6aa2fdff01b7342cd9b12c1fe47aea
xylafur <jkesleyrichardson@gmail.com>
2018-06-14 23:55:28 -0500

    Framework set up, words for fib with py

--- codegolf.sh
+++ codegolf.sh
@@ -4,19 +4,38 @@ courses=("fib")
 languages=("python")
 
 function usage {
-    echo "$0 play <course> <file> <language>"
+    echo "$0 play <file>"
     echo "Courses:"
     echo "    fib"
     echo "languages:"
     echo "    python"
     echo "notes:"
-    echo "    if a compiled languge is used, it is expected that the file is"
-    echo "    precompiled before calling this program.  Then the file param"
-    echo "is ran, gither by the virtual machine or system"
+    echo "  For compiled languages like c, cpp, rust, java, etc.. just include"
+    echo "  the source file, this script will take care of running it."
+    echo
+    echo "  You have to name your source file properly or the script won't be"
+    echo "  able to interpret which course to run"
+    echo
+    echo "  Format:     <coursename>.<file extention>"
+    echo "  Example:    fib.py"
+}
+
+function list_courses {
+    #should implement this dynamically
+    #could have a directory of all of the answer files and then just strip
+    #those
+    for file in ./answers/*
+    do
+        echo $(basename $file)
+    done
+}
+
+function print_course_help {
+    cat ./descriptions/$1
 }
 
 function run_file {
-    case $2 in
+    case $1 in
     #there has to be some way to implement this with arrays to make it 
     #dynamic.. I'll have to look it up later
     python)
@@ -29,36 +48,14 @@ function run_file {
 
 #1: course, #2: file, #3 language
 function play {
-    if ! [[ "$(< ./$1.ans | xargs)" = "$(run_file $3 $2 | xargs)" ]];
+    if ! [[ "$(cat "answers/$1" | xargs)" = "$(run_file $3 $2 | xargs)" ]];
     then
         echo "incorrect! Didn't get the right answer"
         exit
+    else
+        echo "Character count: $(wc -m $2)"
     fi
 
-    echo "Character count: $(echo $2 | wc -c)"
-
-}
-
-function check_course {
-    for course in $courses
-    do
-        if [[ "$1" =~ "$course" ]];
-        then
-            echo 1
-        fi
-    done
-    echo 0
-}
-
-function check_language {
-    for language in $languages
-    do
-        if [[ "$1" =~ "$language" ]];
-        then
-            echo 1
-        fi
-    done
-    echo 0
 }
 
 function get_lang {
@@ -73,54 +70,49 @@ function get_lang {
 
 function get_course {
     pot=$(echo $1 | cut -d. -f1)
-    for course in courses
+
+    for course in answers/*
     do
-        if [ "$pot" = "$course" ];
+        if [ "$pot" = "$(basename $course)" ];
         then
-            echo $pot
-            return
+            echo "$pot"
         fi
     done
-    echo "$pot is not a valid course!"
-    exit 1
 }
 
-
-function argcheck {
-
-    case $1 in
-        play)
-            if [ "$(check_course "$2")" = "0" ];
-            then
-                echo "Not a valid course!"
-                exit 1
-            fi
-
-            if [ "$(check_language "$4")" = "0" ];
-            then
-                echo "Not a valid language!"
-                echo "For accepted languages run: $0 lang"
-                exit 1
-            fi
-
-            if [ -z ${3+x} ]; then
-                echo "Need to supply file !"
-                exit 1
-            fi
-            play $2 $3 $4
-            ;;
-        *)
-            echo "What?"
-            ;;
-    esac
-}
 
 case $1 in
 play)
-    lang=$(get_lang $2)
-    course=$(get_course $2)
-    play $course $2 $lang
-    #argcheck $1 $2 $3 $4
+
+    lang="$(get_lang $2)"
+    course="$(get_course $2)"
+
+    #make checks independent and more verbose
+    if [ -z ${2+z} ];
+    then
+        echo "Need to provide file!"
+        exit 1
+
+    elif [ -z ${course+z} ];
+    then
+        echo "Could not find course for $course"
+        exit 1
+
+    elif [ -z ${lang+z} ];
+    then
+        echo "Could not find language for $lang"
+        exit 1
+    fi
+
+    play "$course" "$2" "$lang"
+    ;;
+courses)
+    if [ ! -z ${2+z} ];
+    then
+        print_course_help $2
+        exit
+    fi
+    list_courses
     ;;
 *)
     echo "What?"
