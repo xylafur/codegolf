30da4b9ebde190cd23a35eec4ae21ac2e011eea4
xylafur <jkesleyrichardson@gmail.com>
2018-06-14 22:11:55 -0500

    Added basic code flow

--- codegolf.sh
+++ codegolf.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-function main {
-    echo main
-}
+courses=("fib")
+languages=("python")
 
 function usage {
     echo "$0 play <course> <file> <language>"
@@ -16,39 +15,75 @@ function usage {
     echo "is ran, gither by the virtual machine or system"
 }
 
-#$1 languge, $2: file
-function runfile {
-    case $1 in
+function run_file {
+    case $2 in
+    #there has to be some way to implement this with arrays to make it 
+    #dynamic.. I'll have to look it up later
     python)
-        python3 $2
+        echo "$(python3 $2)"
         ;;
     *)
-        echo "Unsupported language! Shouldn't get here!!!!")
         ;;
     esac
 }
+
 #1: course, #2: file, #3 language
 function play {
-    if [[ $(< ~/codegolf/$1.ans) != $(runfile $3 $2) ]]; then
-        echo "Incorrect!!!"
-    else
-        echo "Correct!"
+    if ! [[ "$(< ./$1.ans | xargs)" = "$(run_file $3 $2 | xargs)" ]];
+    then
+        echo "incorrect! Didn't get the right answer"
+        exit
     fi
+
+    echo "Character count: $(echo $2 | wc -c)"
+
 }
 
+function check_course {
+    for course in $courses
+    do
+        if [[ "$1" =~ "$course" ]];
+        then
+            echo 1
+        fi
+    done
+    echo 0
+}
+
+function check_language {
+    for language in $languages
+    do
+        if [[ "$1" =~ "$language" ]];
+        then
+            echo 1
+        fi
+    done
+    echo 0
+}
+
+
 function argcheck {
+
     case $1 in
         play)
-            if ! [[ "$2" =~ ^(fib)$ ]]; then
+            if [ "$(check_course "$2")" = "0" ];
+            then
                 echo "Not a valid course!"
+                exit 1
             fi
-            if ! [[ "$4" =~ ^(python)$ ]]; then
+
+            if [ "$(check_language "$4")" = "0" ];
+            then
                 echo "Not a valid language!"
+                echo "For accepted languages run: $0 lang"
+                exit 1
             fi
+
             if [ -z ${3+x} ]; then
                 echo "Need to supply file !"
                 exit 1
             fi
+            play $2 $3 $4
             ;;
         *)
             echo "What?"
@@ -58,7 +93,6 @@ function argcheck {
 
 case $1 in
 play)
-    echo "Playing!"
     argcheck $1 $2 $3 $4
     ;;
 *)
